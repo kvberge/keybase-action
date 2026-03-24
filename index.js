562a3af081d824cc95f1c3a71a0633062a5c73bc
Kevin Vanden Berge <kevin@bright.ai>
2026-03-24 12:43:46 +0000

    feat: upgrade github action runtime to node24 and update dependencies
    update action runtime from node16 to node24 to resolve deprecation warnings on github runners
    
    upgrade @actions/core from 1.6.0 to 3.0.0
    
    upgrade @actions/exec from 1.1.0 to 3.0.0
    
    upgrade @actions/github from 5.0.0 to 9.0.0
    
    Signed-off-by: Kevin Vanden Berge <kevin@bright.ai>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const os = require("os");
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+import os from "os";
+import core from '@actions/core';
+import github from '@actions/github';
+import exec from '@actions/exec';
 
 // Constant strings
 const docker_image = "bjgeiser/keybase-cli:v6.5.1";
